41573dd428a927b3224071999391e23df3ca9fac
kevin.tranchina <kevin.tranchina@gmail.com>
2022-07-10 14:43:24 +0200

    add spring boot servele initializer

--- src/main/java/interview/javaguides/todolist/TodoListManagementSystemApplication.java
+++ src/main/java/interview/javaguides/todolist/TodoListManagementSystemApplication.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import interview.javaguides.todolist.entity.User;
 import interview.javaguides.todolist.repository.UserRepository;
 
 @SpringBootApplication
-public class TodoListManagementSystemApplication implements CommandLineRunner{
+public class TodoListManagementSystemApplication extends SpringBootServletInitializer implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListManagementSystemApplication.class, args);
